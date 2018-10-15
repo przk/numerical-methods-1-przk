e36043600053e0ae38e2d9ae2c6186e29bfd498a
przk <43840987+przk@users.noreply.github.com>
2018-10-15 20:35:33 +0200

    zadanie wykonane

--- src/hadamard.cpp
+++ src/hadamard.cpp
@@ -1,3 +1,16 @@
 #include "hadamard.h"
 
 // Implement your function here.
+//AB[i][j]=A[i][j]*B[i][j]
+
+void hadamard(double **A, double **B, double **AB, int m, int n)
+{
+	for(int i=0; i<m; i++)
+	{
+		for(int j=0; j<n; j++)
+		{
+			AB[i][j]=A[i][j]*B[i][j];
+		}
+	}
+}
+
